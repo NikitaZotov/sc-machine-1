ddc48ecfbc680d438926da2eb21939ce05f494a8
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-03-25 20:06:43 +0300

    [sc-string-tree] Fix size type in get link content

--- sc-memory/sc-core/sc-store/sc_storage.c
+++ sc-memory/sc-core/sc-store/sc_storage.c
@@ -1021,7 +1021,7 @@ sc_result sc_storage_find_links_with_content(const sc_memory_context *ctx, const
   *result_count = 0;
 
   sc_char *sc_string = null_ptr;
-  sc_uint16 size = 0;
+  sc_uint32 size = 0;
   if (sc_link_get_content(stream, &sc_string, &size) != SC_TRUE)
     return SC_RESULT_ERROR;
 
