ostis-ai/sc-machine

[memory] Reuse freed segment elements

sc-memory/sc-core/sc-store/sc_storage.h
@@ -20,7 +20,9 @@ typedef struct
   sc_segment ** segments;
   sc_uint32 segments_count;
   sc_uint32 max_segments_count;
-  sc_monitor_table monitors_table;
+  sc_list * empty_elements;
+  sc_monitor_table addr_monitors_table;
+  sc_monitor segments_monitor;
 } sc_storage;
 
 //! Initialize sc storage in specified path

NikitaZotov/sc-machine-1

sc-memory/sc-core/sc-store/sc_storage.h
@@ -20,7 +20,9 @@ typedef struct
   sc_segment ** segments;
   sc_uint32 segments_count;
   sc_uint32 max_segments_count;
-  sc_monitor_table monitors_table;
+  sc_list * empty_segments;
+  sc_monitor_table addr_monitors_table;
+  sc_monitor segments_monitor;
 } sc_storage;
 
 //! Initialize sc storage in specified path